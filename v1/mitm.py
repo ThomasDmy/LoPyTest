41c9b6dbb876752d64d76538eac4ebc8556b9c5d
Tristan Claverie <public@tclaverie.eu>
2017-11-27 11:20:10 +0100

    Add MitM for version 1

---
+++ v1/mitm.py
@@ -0,0 +1,26 @@
+from network import LoRa
+import socket
+import time
+
+# Man-in-the-Middle LoPy essentially logs all received communication in hexadecimal
+# It stays the same throughout the tests
+
+
+def to_hex(data):
+    """
+        Args:
+            - data: A bytes object
+        Returns:
+            The hexadecimal representation of the data
+    """
+    return ":".join("{:02x}".format(char) for char in data)
+
+
+lora = LoRa(mode=LoRa.LORA, frequency=868100000)
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+s.setblocking(True)
+
+while True:
+    msg = s.recv(255)
+    print("Received {}".format(msg))
+    time.sleep(0.01)
