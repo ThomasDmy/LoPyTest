d000a6665c1daea9f54e8c896915d2eb2cdef969
HUANG Manutea <manutea.huang@insa-rennes.fr>
2017-11-27 09:24:47 +0100

    adding gateway to v1

--- v1/mitm.py
+++ v1/mitm.py
@@ -5,6 +5,7 @@ import time
 # Man-in-the-Middle LoPy essentially logs all received communication in hexadecimal
 # It stays the same throughout the tests
 
+<<<<<<< HEAD
 
 def to_hex(data):
     """
@@ -16,11 +17,17 @@ def to_hex(data):
     return ":".join("{:02x}".format(char) for char in data)
 
 
+=======
+>>>>>>> adding gateway to v1
 lora = LoRa(mode=LoRa.LORA, frequency=868100000)
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 s.setblocking(True)
 
 while True:
     msg = s.recv(255)
+<<<<<<< HEAD
     print("Received {}".format(msg))
+=======
+    print("Received {:x}".format(msg))
+>>>>>>> adding gateway to v1
     time.sleep(0.01)
