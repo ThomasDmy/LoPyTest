e1d0a6dd06808ae3a5220e9e9dc00e9611d2bc0b
Tristan Claverie <public@tclaverie.eu>
2017-11-27 11:19:34 +0100

    Unify printing of messages

--- prelim/test2/mitm.py
+++ prelim/test2/mitm.py
@@ -5,11 +5,22 @@ import time
 # Man-in-the-Middle LoPy essentially logs all received communication in hexadecimal
 # It stays the same throughout the tests
 
+
+def to_hex(data):
+    """
+        Args:
+            - data: A bytes object
+        Returns:
+            The hexadecimal representation of the data
+    """
+    return ":".join("{:02x}".format(char) for char in data)
+
+
 lora = LoRa(mode=LoRa.LORA, frequency=868100000)
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 s.setblocking(True)
 
 while True:
     msg = s.recv(255)
-    print("Received {:x}".format(msg))
+    print("Received {}".format(msg))
     time.sleep(0.01)
